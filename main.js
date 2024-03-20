5b5f1bc759d525b95ba20fd724cc8668dbbd6c0b
perezgigantealex <perezgigantealex@fpllefia.com>
2024-03-20 20:20:45 +0100

    .

--- main.js
+++ main.js
@@ -3,4 +3,19 @@
 function pintaPokemon(array){
 
 }
+const url = 'https://pokeapi.co/api/v2/pokemon'
+document.querySelector('.ejercicio1').addEventListener('click', pokemonAsync)
 
+async function pokemonAsync(){
+    try{
+        const res1 = await fetch(url)
+        const respJ = await res1.json()
+        console.log(respJ)
+        array = respJ.map((id) => id<13)
+        console.log(array)
+        const img = `<img src="${respJ.sprites.front_default}"></img>`
+        document.querySelector('body').innerHTML = img
+    }catch(error){
+        console.log('Error pokemon 1', error)
+    }
+}
